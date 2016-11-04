4c69cfa34cdaa1d7aead474af066b8ad83d02d2a
AaronCritchley <aaron.critchley@gmail.com>
2016-11-04 17:03:22 +0000

    Spaces instead of tabs

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function(){
-	document.getElementsByClassName('main-title')[0].innerHTML = [5,6,0].indexOf((new Date()).getDay()) > -1 ? 'pls no' : 'maybe (probably not)';
+    document.getElementsByClassName('main-title')[0].innerHTML = [5,6,0].indexOf((new Date()).getDay()) > -1 ? 'pls no' : 'maybe (probably not)';
 }
